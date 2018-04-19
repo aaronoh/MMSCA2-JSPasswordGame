f0ed8b262513720eb0fe62810cda792f2516de64
aaronoh <aaronoh96@gmail.com>
2018-04-19 21:04:03 +0100

    Fully implemented progressbar.js - charts drawn for total scores

--- js/checkPassword.js
+++ js/checkPassword.js
@@ -3,13 +3,32 @@ $(document).ready(function(){
     $('.modal').modal();
 });
 
-window.onload = function onLoad() {
-    var chart = new ProgressBar.Circle('#circle', {easing: 'easeInOut'});
-    chart.animate(.8);  // Value from 0.0 to 1.0);
-    chart.setText(8);
-};
 
-function getPw(){
+function getPw() {
+    document.getElementById('chart-container').innerHTML = '                    <div class="col l3">\n' +
+        '                        <div class="card">\n' +
+        '                        <div class="card-content">\n' +
+        '                        <span id ="ctitle" class="card-title">Numbers</span>\n' +
+        '                <div class="chart" id="numcircle"></div>\n' +
+        '                        </div>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                    <div class="col l3">\n' +
+        '                        <div class="card">\n' +
+        '                            <div class="card-content">\n' +
+        '                            <span id ="ctitle" class="card-title">Symbols</span>\n' +
+        '                                <div class="chart" id="symcircle"></div>\n' +
+        '                            </div>\n' +
+        '                        </div>\n' +
+        '                    </div>\n' +
+        '                    <div class="col l3">\n' +
+        '                        <div class="card">\n' +
+        '                            <div class="card-content">\n' +
+        '                            <span id ="ctitle" class="card-title">Total</span>\n' +
+        '                                <div class="chart" id="tcircle"></div>\n' +
+        '                            </div>\n' +
+        '                        </div>\n' +
+        '                    </div>';
     let lengthscore = 0;
     let numscore = 0;
     let symscore = 0;
@@ -17,8 +36,8 @@ function getPw(){
     let pw = document.getElementById('pwInput').value;
     let output = document.getElementById('results');
     let scores = document.getElementById('scores');
-    output.innerHTML='';
-    if (!pw){
+    output.innerHTML = '';
+    if (!pw) {
         window.alert('Please enter a password to test!');
         return;
     }
@@ -31,17 +50,17 @@ function getPw(){
         lengthscore = length;
     }
     //If pw is over 10 chars they get the max score - 10
-    else{
+    else {
         lengthscore = 10;
     }
 
-    if (length <= 5){
+    if (length <= 5) {
         lengthCom = ` This is not an ideal password. You need to add a few more characters.`;
     }
-    if (length > 5 && length < 9){
+    if (length > 5 && length < 9) {
         lengthCom = ` This is an ok password. You would benefit from add a few more characters.`;
     }
-    if (length >= 9){
+    if (length >= 9) {
         lengthCom = ` This password is an ideal length and should be secure.`;
     }
     //-------------------------------------------------------------------------------------------------------------------------------//
@@ -51,19 +70,22 @@ function getPw(){
     //regex
     let numsregex = pw.match(/\d+/g);
     //convert regex obj to str - get length and deduct 4 for ['']
-    let nums = JSON.stringify(numsregex).length-4;
+    let nums = JSON.stringify(numsregex).length - 4;
 
-    if (nums === 0){
+    if (nums === 0) {
         numCom = `Adding a few numbers will  enhance the security of your password.`;
         numscore = 0;
+        numcol = '#ff7675'
     }
-    else if (nums === 1){
+    else if (nums === 1) {
         numCom = ` This is good practice. If you'd like to increase your security you could add an additional number.`;
         numscore = 5;
+        numcol = '#fdcb6e'
     }
-    else if (nums > 1){
+    else if (nums > 1) {
         numCom = ` A variety of numbers increases the security of your password.`;
         numscore = 10;
+        numcol = '#00b894'
     }
     //----------------------------------------------------------------------------------------------------------------------------//
 
@@ -71,19 +93,22 @@ function getPw(){
     //regex
     let symaregex = pw.match(/\W+/);
 
-    let syms = JSON.stringify(symaregex).length-4;
+    let syms = JSON.stringify(symaregex).length - 4;
 
-    if (syms === 0){
+    if (syms === 0) {
         symCom = `Adding a few symbols will greatly improve your password.`;
         symscore = 0;
+        symcol = '#ff7675'
     }
-    else if (syms === 1){
-        numCom = ` This is good practice. If you'd like to increase your security you could add an additional symbol.`;
+    else if (syms === 1) {
+        symCom = ` This is good practice. If you'd like to increase your security you could add an additional symbol.`;
         symscore = 5;
+        symcol = '#fdcb6e'
     }
-    else if (syms > 1){
+    else if (syms > 1) {
         symCom = `Well done!`;
         symscore = 10;
+        symcol = '#00b894'
     }
     //--------------------------------------------------------------------------------------------------------------------------------//
 
@@ -91,8 +116,53 @@ function getPw(){
     //------------------------------------------------------Results-------------------------------------------------------------------//
     let results = `Your password is: <span style="font-weight: bold; margin-left: 5px;">  ${pw}.</span> It is ${length} characters long. ${lengthCom} It contains ${nums} numbers. ${numCom} It also contains ${syms} symbols. ${symCom}`;
     let totalscore = lengthscore + numscore + symscore;
-    let percentagescore = Math.floor(totalscore/30 *100);
-    output.innerHTML=results;
-    scores.innerHTML=`Your total score is ${totalscore}/30. You scored ${lengthscore}/10 in length, ${numscore}/10 in numbers and ${symscore}/10 in symbols. That's ${percentagescore}%`;
+    let percentagescore = Math.floor(totalscore / 30 * 100);
+    output.innerHTML = results;
+    scores.innerHTML = `Your total score is ${totalscore}/30. You scored ${lengthscore}/10 in length, ${numscore}/10 in numbers and ${symscore}/10 in symbols. That's ${percentagescore}%`;
     //--------------------------------------------------------------------------------------------------------------------------------//
+
+    var numchart = new ProgressBar.Circle('#numcircle', {
+        color: numcol,
+        duration: 2000,
+        easing: 'easeInOut'
+    });
+    if (numscore > 0) {
+        numchart.animate(numscore / 10);
+    }
+    else {
+        numchart.animate(0.01);
+    }
+    numchart.setText(numscore * 10 + '%');
+
+
+    var symchart = new ProgressBar.Circle('#symcircle', {
+        color: symcol,
+        duration: 2000,
+        easing: 'easeInOut'
+    });
+    if (numscore > 0) {
+        symchart.animate(symscore / 10);
+    }
+    else {
+        symchart.animate(0.01);
+    }
+    symchart.setText(symscore * 10 + '%');
+
+    if (totalscore <= 10){
+        tcol = '#ff7675'
+    }
+    if (totalscore >10 && totalscore <= 20){
+        tcol = '#fdcb6e'
+    }
+    if (totalscore > 20 && totalscore <= 30){
+        tcol = '#00b894'
+    }
+    var totalchart = new ProgressBar.Circle('#tcircle', {
+        color: tcol,
+        duration: 2000,
+        easing: 'easeInOut'
+    });
+    totalchart.animate(percentagescore / 100);
+
+    totalchart.setText(percentagescore + '%');
 }
